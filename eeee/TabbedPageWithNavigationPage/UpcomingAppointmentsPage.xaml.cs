ad38958ab4578e228b1a3c1291254a6bb7850d98
XfHua <jasonHuaxf@163.com>
2019-05-22 13:26:09 +0800

    132

---
+++ eeee/TabbedPageWithNavigationPage/UpcomingAppointmentsPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace TabbedPageWithNavigationPage
+{
+	public partial class UpcomingAppointmentsPage : ContentPage
+	{
+		public UpcomingAppointmentsPage ()
+		{
+			InitializeComponent ();
+		}
+
+		async void OnBackButtonClicked (object sender, EventArgs e)
+		{
+			await Navigation.PopAsync ();
+		}
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            //Clear Navigation Stack, clicking on tab page should always 
+            //go to corresponding page
+
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                try
+                {
+                    var existingPages = Navigation.NavigationStack.ToList();
+                    foreach (var page in existingPages)
+                    {
+                        //existingPages count should be greater than 1, so that this will never be root page. Otherwise removing root page will throw exception
+                        if (string.Compare(page.GetType().Name, "UpcomingAppointmentsPage", StringComparison.OrdinalIgnoreCase) == 0 && existingPages.Count > 1)
+                        {
+                            Navigation.RemovePage(page);
+                        }
+
+                    }                 
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }           
+        }
+
+    }
+}
+
---
+++ eeee/iOS/myTabbarRenderer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using TabbedPageWithNavigationPage;
+using TabbedPageWithNavigationPage.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+
+[assembly: ExportRenderer(typeof(MainPage), typeof(myTabbarRenderer))]
+namespace TabbedPageWithNavigationPage.iOS
+{
+    class myTabbarRenderer : TabbedRenderer
+    {
+        private MainPage _page;
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                _page = (MainPage)e.NewElement;
+            }
+            else
+            {
+                _page = (MainPage)e.OldElement;
+            }
+
+            try
+            {
+                var tabbarController = (UITabBarController)this.ViewController;
+                if (null != tabbarController)
+                {
+                    tabbarController.ViewControllerSelected += OnTabbarControllerItemSelected;
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        private async void OnTabbarControllerItemSelected(object sender, UITabBarSelectionEventArgs eventArgs)
+        {
+            if (_page?.CurrentPage?.Navigation != null && _page.CurrentPage.Navigation.NavigationStack.Count > 0)
+            {
+                await _page.CurrentPage.Navigation.PopToRootAsync();
+            }
+
+        }
+    }
+}
